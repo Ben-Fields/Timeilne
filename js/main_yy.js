6ca6964086c37bc75ee7ef7b7d0ef883366cc576
Ben-Fields <33070053+Ben-Fields@users.noreply.github.com>
2021-10-29 11:05:57 -0500

    add tabs, namespace bootstrap

--- js/main_yy.js
+++ js/main_yy.js
@@ -1,40 +1,40 @@
 let selectEditTab = function (evt, tabID) {
+	tablinks = evt.target.parentNode.getElementsByClassName("panel-title");
+	for (i = 0; i < tablinks.length; i++) {
+	  tablinks[i].className = "panel-title panel-title-inactive";
+	  evt.currentTarget.className 
+	}
 
-  tablinks = document.getElementsByClassName("tablink");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
+	tabcontents = document.getElementsByClassName("tabcontent");
+	for (i = 0; i < tabcontents.length; i++) {
+	  tabcontents[i].style.display = "none";
+	}
 
-  tabcontents = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontents.length; i++) {
-    tabcontents[i].style.display = "none";
-  }
-
-  evt.currentTarget.className += " active";
-  document.getElementById(tabID).style.display = "block";
+	evt.currentTarget.className = evt.currentTarget.className.replace(" panel-title-inactive", "");
+	document.getElementById(tabID).style.display = "block";
 }
 
 var cur_floating_icon;
 
 let clickEditElement = function (evt, target_id) {
-  if(cur_floating_icon){
-    cur_floating_icon.style.display = "none";
-  }
-  cur_floating_icon = document.getElementById(target_id);
-  cur_floating_icon.style.display = "block";
-  cur_floating_icon.style.left = (evt.clientX + 10) + "px";
-  cur_floating_icon.style.top = (evt.clientY + 10) + "px";
+	if(cur_floating_icon){
+	  cur_floating_icon.style.display = "none";
+	}
+	cur_floating_icon = document.getElementById(target_id);
+	cur_floating_icon.style.display = "block";
+	cur_floating_icon.style.left = (evt.clientX + 10) + "px";
+	cur_floating_icon.style.top = (evt.clientY + 10) + "px";
 }
 
 $(document).mousemove(function (e) {
-  if(!cur_floating_icon){
-    return
-  }
-  cur_floating_icon.style.left = (e.clientX + 10) + "px";
-  cur_floating_icon.style.top = (e.clientY + 10) + "px";
+	if(!cur_floating_icon){
+	  return
+	}
+	cur_floating_icon.style.left = (e.clientX + 10) + "px";
+	cur_floating_icon.style.top = (e.clientY + 10) + "px";
 });
 
 $("#panel-timeline").mousedown(function (e) {
-  cur_floating_icon.style.display = "none";
-  cur_floating_icon = null;
+	cur_floating_icon.style.display = "none";
+	cur_floating_icon = null;
 });
