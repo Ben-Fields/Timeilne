95909e6c8cdc2d58ba7ffc2e2f4a2e99ddc3a557
Yatharth <83032264+multimodal-interaction-lab@users.noreply.github.com>
2021-11-03 18:47:12 -0500

    Add group

--- js/main_yath.js
+++ js/main_yath.js
@@ -1,6 +1,19 @@
 function delete_group()
 {
 	var element = document.querySelector("#group-section > ul > li.active"); 
-	//console.log(element);
 	element.style.display = "none";
+}
+
+function add_group()
+{
+	var grp_name = document.getElementById("group-name").value;
+	console.log(grp_name);
+	const text_node = document.createTextNode(grp_name);
+	var parent_ul = document.querySelector("#group-section > ul");
+	const li = document.createElement("li");
+	const a = document.createElement("a")
+	a.setAttribute("data-toggle", "pill");
+	a.appendChild(text_node);
+	li.appendChild(a);
+	parent_ul.appendChild(li);
 }
