90cd0b8bb0f7eec1988dd028c3da57101737ceb2
Yatharth <ysinghal2209@gmail.com>
2021-11-10 12:05:44 -0600

    Info Tab

--- js/main_yath.js
+++ js/main_yath.js
@@ -206,4 +206,14 @@ window.addEventListener('beforeunload', (event) => {
   	event.returnValue = `Are you sure you want to leave?`;
 });
 
+suggestions_list = ["Sample format can be downloaded using the Format button in the Files Tab", "Mouse Left can be used to navigate through the timeline", "Mouse Scroll can be used to zoom-in and out on the timeline"]
 
+function changeInfo()
+{
+	setInterval(function()
+	{ 
+		var randomNumber = Math.floor(Math.random() * suggestions_list.length);
+		console.log(randomNumber);
+		document.getElementById("info_messages").innerHTML = suggestions_list[randomNumber];
+	}, 3000);
+}
